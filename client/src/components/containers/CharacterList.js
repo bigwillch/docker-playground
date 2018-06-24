84062e2d31f15ebdfb25230d883d61ed68db6098
Will Howlett <wehowlett@gmail.com>
2018-06-24 12:36:18 +0100

    hook up search field

--- client/src/components/containers/CharacterList.js
+++ client/src/components/containers/CharacterList.js
@@ -1,41 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
 import Character from 'Presentational/Character'
 
-const CharacterList = () => (
-  <Query
-    query={gql`
-      {
-        characters {
-          name
-          img
-        }
-      }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :(</p>;
+const GET_CHARACTERS = gql`
+  query SearchCharacters($name: String) {
+    characters(name: $name) {
+      name
+      img
+    }
+  }
+`
 
-      const chars = data.characters.map((item, index) =>
-        <Character
-          key={index}
-          name={item.name}
-          img={item.img}
-        />
-      );
+class CharacterList extends Component {
 
-      return (
-        <React.Fragment>
-          {chars.length > 0 &&
-            chars
-          }
-        </React.Fragment>
-      );
-    }}
-  </Query>
-);
+  state = {
+    filter: '',
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <input
+            type='text'
+            placeholder='Search for...'
+            onChange={(e) => this.setState({ filter: e.target.value })}
+          />
+          {/* <button onClick={this.clearSearch}>Clear</button> */}
+        </div>
+
+        <Query
+          query={GET_CHARACTERS}
+          variables={{ name: this.state.filter}}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+
+            const chars = data.characters.map((item, index) =>
+              <Character
+                key={index}
+                name={item.name}
+                img={item.img}
+              />
+            );
+
+            return (
+              <React.Fragment>
+                {chars.length > 0 &&
+                  chars
+                }
+              </React.Fragment>
+            );
+          }}
+        </Query>
+      </React.Fragment>
+    )
+  }
+}
 
 export default CharacterList
